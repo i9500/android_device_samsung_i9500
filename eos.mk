2f40ac4d78ee3a9e735faaeb38429f6ebab594e5
BlaqueSoul <bryne@blaquesoul.com>
2015-08-27 18:11:00 +0200

    Update and rename cm.mk to eos.mk

--- eos.mk
+++ eos.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2015 EuphoriaOS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,13 +18,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from i9500 device
 $(call inherit-product, device/samsung/i9500/i9500.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/eos/config/common.mk)
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-PRODUCT_NAME := cm_i9500
+PRODUCT_NAME := eos_i9500
 PRODUCT_DEVICE := i9500
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-I9500
@@ -38,5 +36,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 	TARGET_DEVICE=ja3g \
 	BUILD_FINGERPRINT="samsung/ja3gxx/ja3g:5.0.1/LRX22C/I9500XXUHOG1:user/release-keys" \
 	PRIVATE_BUILD_DESC="ja3gxx-user 5.0.1 LRX22C I9500XXUHOG1 release-keys"
-
-TARGET_UNOFFICIAL_BUILD_ID := GearCM
+	
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
